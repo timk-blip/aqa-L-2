0330eb2b5f690ef9d169fb3eeb36377ffd02ad67
Timur Kiselev <snygens@mail.ru>
2020-08-24 16:37:01 +0300

    add all project

---
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -0,0 +1,33 @@
+package ru.netology.rest;
+
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.*;
+
+class MobileBankApiTestV3 {
+    @Test
+    void shouldReturnDemoAccounts() {
+      // Given - When - Then
+      // Предусловия
+      given()
+          .baseUri("http://localhost:9999/api/v1")
+      // Выполняемые действия
+      .when()
+          .get("/demo/accounts")
+      // Проверки
+      .then()
+              .log().all()
+              .statusCode(200)
+              // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
+              .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+          .contentType(ContentType.JSON)
+          .body("", hasSize(3))
+          .body("[0].currency", equalTo("RUB"))
+          .body("[0].balance", greaterThanOrEqualTo(0))
+      ;
+    }
+}
